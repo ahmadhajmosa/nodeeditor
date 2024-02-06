c484a2f0a35389ca6b91f7824d68d43980057d7c
ahmadhajmosagitlab <ahmad.haj.mosa@pwc.com>
2024-02-06 18:51:19 +0100

    changed boarder colors

--- src/example/nodes-and-edges.ts
+++ src/example/nodes-and-edges.ts
@@ -66,8 +66,5 @@ export const initialEdges: Edge[] = [
   { id: 'e22a', source: '2', target: '2a', type: edgeType, animated: true },
   { id: 'e22b', source: '2', target: '2b', type: edgeType, animated: true },
   { id: 'e22c', source: '2', target: '2c', type: edgeType, animated: true },
-  { id: 'e2c2d', source: '2c', target: '2d', type: edgeType, animated: true },
-  { id: 'e45', source: '4', target: '5', type: edgeType, animated: true },
-  { id: 'e56', source: '5', target: '6', type: edgeType, animated: true },
-  { id: 'e57', source: '5', target: '7', type: edgeType, animated: true }
+  { id: 'e2c2d', source: '2c', target: '2d', type: edgeType, animated: true }
 ];
